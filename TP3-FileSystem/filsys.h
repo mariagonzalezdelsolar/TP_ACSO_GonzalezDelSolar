cd8db2c6a7e7f7b0bde869f837c34250dae9fdf2
mariagonzalezdelsolar <126919162+mariagonzalezdelsolar@users.noreply.github.com>
2025-05-13 14:22:19 -0300

    Update filsys.h

--- TP3-FileSystem/filsys.h
+++ TP3-FileSystem/filsys.h
@@ -17,8 +17,6 @@
  * routines for free list and I list.
  */
 
- #define BLOCK_SIZE 512
-
 struct filsys {
   uint16_t	s_isize;	// size in blocks of I list
   uint16_t	s_fsize;	// size in blocks of entire volume
