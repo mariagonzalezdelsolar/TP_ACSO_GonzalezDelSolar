d1014a0816acc9b268df18c5da69fc69a50a5cc1
mariagonzalezdelsolar <gonzalezdelsolaem@udesa.edu.ar>
2025-05-13 10:28:07 -0300

    final

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -6,11 +6,47 @@
 #include <string.h>
 #include <assert.h>
 
-/**
- * TODO
- */
-int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+int directory_findname(struct unixfilesystem *fs, const char *name, int dirinumber, struct direntv6 *dirEnt) {
+  // Verifico punteros nulos
+  if (fs == NULL || name == NULL || dirEnt == NULL) {
+    return -1; // Error: puntero nulo
+  }
+  struct inode dir_inode;
+
+  // MANEJO DE ERRORES
+  // Errores de inodos
+  if (inode_iget(fs, dirinumber, &dir_inode) < 0) {
+    return -1; // Error al leer el inodo del directorio.
+  }
+  if ((dir_inode.i_mode & IALLOC) == 0) {
+    return -1; // Inodo no asignado.
+  }
+  // Errores de directorio
+  if ((dir_inode.i_mode & IFDIR) == 0) {
+    return -2; // No es un directorio.
+  }
+
+  char block[BLOCK_SIZE];
+  int num_blocks = (inode_getsize(&dir_inode) + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+  // Error al leer el bloque del directorio.
+  for (int blk = 0; blk < num_blocks; blk++) {
+    int bytes_read = file_getblock(fs, dirinumber, blk, block);
+    if (bytes_read <= 0) {
+        return -3;
+    }
+  
+  // Cada bloque contiene varias entradas de directorio.
+  int num_entries = BLOCK_SIZE / sizeof(struct direntv6);
+  struct direntv6 *entries = (struct direntv6 *)block;
+
+  for (int i = 0; i < num_entries; i++) {
+    if (entries[i].d_inumber != 0 && strncmp(entries[i].d_name, name, sizeof(entries[i].d_name)) == 0) {
+      *dirEnt = entries[i]; // Copio la entrada encontrada.
+      return 0; // Éxito.
+    }
+   }
+  }
+
+  return -4; // No se encontró el nombre.
 }
