1f59315008f2f25fedbafde781faffdb51587dc1
mariagonzalezdelsolar <126919162+mariagonzalezdelsolar@users.noreply.github.com>
2025-05-13 14:17:09 -0300

    Update pathname.c

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -7,10 +7,11 @@
 #include <string.h>
 #include <assert.h>
 
+
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    // Verifico punteros nulos
-    if (fs == NULL || pathname == NULL) {
-        return -1; // Error: puntero nulo
+    // Verifico hiperparámetros 
+    if (fs == NULL || pathname == NULL || *pathname == '\0') {
+        return -1; 
     }
 
     // Manejo del caso especial del directorio raíz.
@@ -19,8 +20,13 @@ int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
     }
 
     // Verifico que el path no esté vacío o mal formado.
-    if (pathname[0] != '/' || strlen(pathname) == 1) {
+    if (pathname[0] != '/' || strlen(pathname) < 2) {
         return -1; // Error: path inválido.
+    }
+
+    // Verifico componentes vacíos
+    if (strstr(pathname, "//") != NULL) {
+        return -1;
     }
 
     // Copio el path para evitar modificar el original.
@@ -33,12 +39,18 @@ int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
 
     while (component != NULL) { // Recorro los componentes del path.
         struct direntv6 dir_entry;
+
+        // Chequeo que el nombre del componente no sea demasiado largo
+        if (strlen(component) > DIRENT_NAME_LEN) {
+            return -1; 
+        }
+
         int result = directory_findname(fs, component, current_inumber, &dir_entry);
         
         if (result < 0) {
             return -1; // Error: Componente no encontrado o error de acceso.
         }
-        
+
         current_inumber = dir_entry.d_inumber;
         component = strtok(NULL, "/");
     }
